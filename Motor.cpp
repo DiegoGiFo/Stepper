1f349aaa11e29747e65388022abc20e81fc97cc9
Diego <diego.gibellofoglio@gmail.com>
2018-03-29 15:16:11 +0200

    Code writing

---
+++ Motor.cpp
@@ -0,0 +1,32 @@
+#include "Motor.hpp"
+#include <AccelStepper.h>
+
+const float LAMBDA = 0.999f;
+
+int get_direction(float x)
+{
+    if(x > 0) return 1;
+    else return -1;
+}
+
+Motor::Motor(int step, int dir) {
+    this->target_w = 0.0;
+    this->current_w = 0.0;
+    this->step_mt(AccelStepper::DRIVER, step, dir);
+    this->step_mt.setMaxSpeed(10000);
+}
+
+float Motor::get_speed() {
+  return this->current_w;
+}
+
+void Motor::set_speed(float speed) {
+  this->target_w = speed;
+}
+
+void Motor::run() {
+  this->current_w = LAMBDA*this->current_w + (1-LAMBDA)*this->target_w;
+  const float w_abs = fabs(this->current_w)*(100.0/M_PI);
+  const float direction = get_direction(this->current_w);
+  this->step_mt.setSpeed(direction*w_abs);
+}
---
+++ Motor.hpp
@@ -0,0 +1,20 @@
+#ifndef __MOTOR__HPP__
+#define __MOTOR__HPP__
+
+#include <AccelStepper.h>
+
+class Motor {
+  public:
+    Motor(int step, int dir);
+
+    float get_speed();
+    void set_speed(float speed);
+    void run();
+
+  private:
+    float current_w;
+    float target_w;
+    AccelStepper step_mt;
+};
+
+#endif
