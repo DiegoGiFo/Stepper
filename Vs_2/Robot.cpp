d87395aa0989a6833f3f59c19812b5d314866df7
Diego <diego.gibellofoglio@gmail.com>
2018-04-12 12:28:21 +0200

    Up code

--- Vs_2/Robot.cpp
+++ Vs_2/Robot.cpp
@@ -5,7 +5,7 @@
 #include "Robot.hpp"
 
 #define L 0.160f // distance between the two wheels of the robot
-#define R 0.033f //radius of the wheel od the robot
+#define R 0.040f //radius of the wheel od the robot
 
 const float A = L/(2.0f*R);
 const float B = 1.0f/R;
